74098919ee6b01dcf0c98910081a61bb3caaf345
duongtranngoc <duongtn0306@gmail.com>
2024-01-06 04:10:55 +0700

    delete cv

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../assets/img/my-cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -23,7 +22,6 @@ function Resume() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -33,7 +31,7 @@ function Resume() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          <Document className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 800 ? 1.8 : 0.6} />
           </Document>
         </Row>
@@ -41,7 +39,6 @@ function Resume() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
